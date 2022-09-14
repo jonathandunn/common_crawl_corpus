c349a7deed19a4ed2e3e961d90ea2a907bc05b76
nathan <tnh33@uclive.ac.nz>
2022-09-14 18:46:54 +1200

    Added utilities case to hold constant, added processing individual wet record, dedupe and save to data frame, added requirements.txt

--- common_crawl_corpus/common_crawl_processing.py
+++ common_crawl_corpus/common_crawl_processing.py
@@ -1,36 +1,135 @@
+from typing import Optional
+from alphabet_detector import AlphabetDetector
+from cytoolz import pipe, juxt
+from gensim.parsing import preprocessing
+
+import pickle
 import requests
 import os
 import warcio
+import re
+import tldextract
+import utilities
+import pandas as pd
 
-FILE_DOWNLOAD_DIR = "/home/nhan/Downloads/"
+FILE_DOWNLOAD_DIR = "./"
+# FILE_DOWNLOAD_DIR = "/Users/nathan/Downloads"
+
+alphabet_detector = AlphabetDetector()
 
 
-def download_from_period(index_level: int, segment: str) -> None:
-    """
-    Download a first index path from the given period in common crawl.
-    :arg
-    :param index_level: 1 -
-    :param segment: For example, CC-MAIN-2014-15
-    """
-    if index_level == 1:
-        url = f"https://commoncrawl.org/crawl-data/{segment}/warc.paths.gz"
-    elif index_level == 2:
-        url = f"https://commoncrawl.org/{segment}"
-    else:
-        raise ValueError(f"Invalid index level: {index_level}, index level should be between 1 and 2")
-    with open(os.path.join(FILE_DOWNLOAD_DIR, f"{segment}-warc.paths.gz"), "wb") as file:
+def download_index(year_range: str):
+    url = f"https://data.commoncrawl.org/crawl-data/{year_range}/wet.paths.gz"
+    with open(os.path.join(FILE_DOWNLOAD_DIR, f"{year_range}-warc.paths.gz"), "wb") as file:
         response = requests.get(url)
         file.write(response.content)
 
 
-def read_warc(filename: str) -> None:
-    i = 0
+def download_wet(index: str):
+    url = f"https://data.commoncrawl.org/{index}"
+    with open(os.path.join(FILE_DOWNLOAD_DIR, index.replace("/", "-")), "wb") as file:
+        response = requests.get(url)
+        file.write(response.content)
+
+
+def read_wet(filename: str) -> None:
+    # TODO: add multiprocessing for wet
     with open(os.path.join(FILE_DOWNLOAD_DIR, filename),
               "rb") as file:
+        lines = []
         for record in warcio.ArchiveIterator(file):
-            if record.rec_type == 'response':
-                print(record.rec_headers.get_header('WARC-Target-URI'))
+            if temp := extract_wet_record(record):
+                lines.extend(temp)
+
+        df = pd.DataFrame(lines)
+        deduplicate(df)
+        save_df(df, filename=filename.replace("/", ".") + ".processed")
+
+
+def extract_wet_record(wrac_record) -> Optional[list[tuple[str, str, str, int, str]]]:
+    """Process individual WRAC record in WET file, return list of
+    url_suffix, current_country, current_region, url, line"""
+    if wrac_record.rec_type != "conversion":
+        return
+    # emoji remover
+    try:
+        emoji_regex = re.compile(u'['u'\U0001F300-\U0001F64F'u'\U0001F680-\U0001F6FF'u'\u2600-\u26FF\u2700-\u27BF]+',
+                                 re.UNICODE)
+    except re.error:
+        emoji_regex = re.compile(
+            u'('u'\ud83c[\udf00-\udfff]|'u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'u'[\u2600-\u26FF\u2700-\u27BF])+',
+            re.UNICODE)
+    illegal_chars = ["|", "©", "«", "®", "»", "˂", "˃", "˄", "˅", "/", "\\", "{", "}"]
+    url: str = wrac_record.rec_headers.get_header("WARC-Target-URI")
+    # getting domain abc.example.com -> ExtractResult(subdomain='abc', domain='hostname', suffix='com')
+    url_domain = tldextract.extract(url)
+    url_suffix = url_domain.suffix
+    # TODO There are bugs where the tldextract url of trademe.co.nz would have the suffix of 'co.nz'
+    if url_suffix not in utilities.COUNTRY_CODES_NAME.keys():
+        return
+    current_country = utilities.COUNTRY_CODES_NAME[url_suffix]
+
+    web_content: str = wrac_record.content_stream().read().decode("utf-8")
+    # we need the line larger than 15 character
+    processed_line: list[tuple[str, str, str, int, str]] = []
+    line_num = 0  # flag to make sure it is the same page
+    for line in web_content.splitlines():
+        if len(line) <= 15:
+            continue
+        # Remove links, hashtags, at-mentions, mark-up, and "RT"
+        line = re.sub(r"http\S+", "", line)
+        line = re.sub(r"@\S+", "", line)
+        line = re.sub(r"#\S+", "", line)
+        line = re.sub("<[^>]*>", "", line)
+
+        # Remove emojis
+        line = re.sub(emoji_regex, "", line)
+
+        # Remove extra spaces
+        line = pipe(line,
+                    preprocessing.strip_tags, preprocessing.split_alphanum, preprocessing.strip_multiple_whitespaces)
+        # Check if still above 15 and not contains any navigational / boilerplate characters
+        if len(line) <= 15 or any(char in line for char in illegal_chars):
+            continue
+        # Check if mostly numbers / characters
+        character_only: str = pipe(line, preprocessing.strip_numeric, preprocessing.strip_punctuation)
+        if len(character_only) <= 12:
+            continue
+        # Check if line has Chinese / Japanese / Korean characters, then set length to 15:
+        if any(juxt(alphabet_detector.is_cjk,
+                    alphabet_detector.is_hangul,
+                    alphabet_detector.is_hiragana,
+                    alphabet_detector.is_katakana
+                    )(line)):
+            length = 15
+        else:
+            length = 50
+        if len(line) < length:
+            continue
+        if all([line.count("-") < 4, line.count("(") < 4, line.count(")") < 4, line.count("=") < 2, line.count("_") < 2,
+                line.count(".") < 15, line.count("&") < 4, line.count("[") < 3, line.count("]") < 3,
+                line.count("*") < 5]):
+            line_num += 1
+            processed_line.append((url_suffix, current_country, url, line_num, line))
+    return processed_line
+
+
+def deduplicate(df: pd.DataFrame):
+    # TODO This deduplication only consider exact duplicate, which is not usually applicable in practice,
+    #  maybe looking in to Levenshtein distance: TheFuzz, jellyfish or difflib.SequenceMatcher
+    df.columns = ["Domain", "Country", "URL", "LineID", "Text"]
+    original_len = len(df.index)
+    df.drop_duplicates(subset="Text", inplace=True)
+    print(f"Formatted and Removed {original_len - len(df.index)} with total: {len(df.index)}")
+
+
+def save_df(df: pd.DataFrame, filename: str):
+    df.infer_objects()
+    # HIGHEST_PROTOCOL for python3.8+, DEFAULT_PROTOCOL for 3.4+
+    df.to_pickle(filename, compression="gzip", protocol=pickle.HIGHEST_PROTOCOL)
+    print(f"Saved {filename}")
 
 
 if __name__ == '__main__':
-    read_warc("CC-MAIN-20140416005201-00000-ip-10-147-4-33.ec2.internal.warc")
+    # download_wet("crawl-data/CC-MAIN-2014-15/segments/1397609521512.15/wet/CC-MAIN-20140416005201-00000-ip-10-147-4-33.ec2.internal.warc.wet.gz")
+    read_wet("CC-MAIN-20211126224056-20211127014056-00000.warc.wet")

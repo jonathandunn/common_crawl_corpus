214f4f95d07e1cf9aa2b1172ccc51a5287626a1c
nathan <nhanhuynh296@gmail.com>
2023-02-07 22:51:40 +1300

    Added downloaded dir default path

--- common_crawl_corpus/cc_corpus.py
+++ common_crawl_corpus/cc_corpus.py
@@ -6,7 +6,7 @@ import multiprocessing as mp
 import os
 from functools import partial
 from multiprocessing.pool import ThreadPool
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Union
 
 import pandas as pd
 import requests
@@ -197,7 +197,10 @@ def process_lid(segment, input_dir, output_dir):
 
 class CC_Corpus(object):
 
-    def __init__(self, countries_to_skip=None, url_filter: str = None, download_dir: str = None):
+    def __init__(self,
+                 countries_to_skip=None,
+                 url_filter: Optional[Union[str, os.PathLike, bytes]] = None,
+                 download_dir: Union[str, os.PathLike, bytes] = "./common_crawl_download"):
 
         # Ignore certain countries if there is already enough data
         if countries_to_skip is None:
@@ -210,10 +213,7 @@ class CC_Corpus(object):
             self.url_filter = utilities.get_url_filters_from_file(url_filter).keys()
 
         # Download directory
-        if download_dir is not None:
-            self.download_dir = download_dir
-        else:
-            self.download_dir = "./common_crawl_download"
+        self.download_dir = download_dir
 
         # Detecting language based on alphabet
         self.alphabet_detector = AlphabetDetector()
@@ -400,62 +400,70 @@ class CC_Corpus(object):
         pool_instance.close()
         pool_instance.join()
 
-        # --------------------------------------------------------------------
+        # ----------------------------------------------------------------------------------------------------------------------#
+
+    def scan_url_filters(self, dataframe_dir: Union[str, os.PathLike, bytes]):
+        # todo get url filter from file
+        df = pd.read_feather(dataframe_dir)
+        # todo scan url from each dataframe
+            # todo keep track of the domain and its language, if domain contain more than 3 language, add it onto the file
+        # todo pickle the dictionary so that we can keep track of the number of language each domian have, and how many pages it got
+        pass
 
     def final_cc(self, input_dir, output_dir, region):
 
-        for country in os.listdir(os.path.join(input_dir, region)):
-            for language in os.listdir(os.path.join(input_dir, region, country)):
+            for country in os.listdir(os.path.join(input_dir, region)):
+                for language in os.listdir(os.path.join(input_dir, region, country)):
 
-                first_flag = True  # First for this set
-                counter = 1
+                    first_flag = True  # First for this set
+                    counter = 1
 
-                for file in os.listdir(os.path.join(input_dir, region, country, language)):
+                    for file in os.listdir(os.path.join(input_dir, region, country, language)):
 
-                    file = os.path.join(input_dir, region, country, language, file)
-                    new_df = pd.read_csv(file, compression="gzip")
+                        file = os.path.join(input_dir, region, country, language, file)
+                        new_df = pd.read_csv(file, compression="gzip")
 
-                    if first_flag == True:
-                        first_flag = False
-                        current_df = new_df
-                        print("\tFirst time for " + region + " " + country + " " + language)
+                        if first_flag == True:
+                            first_flag = False
+                            current_df = new_df
+                            print("\tFirst time for " + region + " " + country + " " + language)
 
-                    else:
+                        else:
 
-                        # First, merge new_df
-                        print("\tContinuing with " + file)
-                        current_df = pd.concat([current_df, new_df])
-                        current_df.drop_duplicates(subset="URL", keep="first", inplace=False)
+                            # First, merge new_df
+                            print("\tContinuing with " + file)
+                            current_df = pd.concat([current_df, new_df])
+                            current_df.drop_duplicates(subset="URL", keep="first", inplace=False)
 
-                        # Second, check length
-                        if len(current_df) > 100000:
-                            write_df = current_df.head(n=100000)
-                            current_df = current_df.tail(n=len(current_df) - 100000)
+                            # Second, check length
+                            if len(current_df) > 100000:
+                                write_df = current_df.head(n=100000)
+                                current_df = current_df.tail(n=len(current_df) - 100000)
 
-                            write_name = region + "." + country + "." + language + "." + str(counter) + ".gz"
-                            write_name = os.path.join(output_dir, region, country, language, write_name)
-                            os.makedirs(os.path.join(output_dir, region, country, language), exist_ok=True)
-                            counter += 1
+                                write_name = region + "." + country + "." + language + "." + str(counter) + ".gz"
+                                write_name = os.path.join(output_dir, region, country, language, write_name)
+                                os.makedirs(os.path.join(output_dir, region, country, language), exist_ok=True)
+                                counter += 1
 
-                            write_df.to_csv(write_name, header=True, index=False, index_label=False,
-                                            compression="gzip")
-                            print("\t\tWriting " + write_name)
-                            del write_df
+                                write_df.to_csv(write_name, header=True, index=False, index_label=False,
+                                                compression="gzip")
+                                print("\t\tWriting " + write_name)
+                                del write_df
 
-                # Done with all files, write the remaining
-                write_name = region + "." + country + "." + language + "." + str(counter) + ".gz"
-                write_name = os.path.join(output_dir, region, country, language, write_name)
-                os.makedirs(os.path.join(output_dir, region, country, language), exist_ok=True)
-                counter += 1
+                    # Done with all files, write the remaining
+                    write_name = region + "." + country + "." + language + "." + str(counter) + ".gz"
+                    write_name = os.path.join(output_dir, region, country, language, write_name)
+                    os.makedirs(os.path.join(output_dir, region, country, language), exist_ok=True)
+                    counter += 1
 
-                current_df.to_csv(write_name, header=True, index=False, index_label=False, compression="gzip")
-                print("\t\tWriting " + write_name)
+                    current_df.to_csv(write_name, header=True, index=False, index_label=False, compression="gzip")
+                    print("\t\tWriting " + write_name)
 
-                del current_df
+                    del current_df
 
-                # Done, now delete
-                for file in os.listdir(os.path.join(input_dir, region, country, language)):
-                    file = os.path.join(input_dir, region, country, language, file)
-                    os.remove(file)
+                    # Done, now delete
+                    for file in os.listdir(os.path.join(input_dir, region, country, language)):
+                        file = os.path.join(input_dir, region, country, language, file)
+                        os.remove(file)
 
-                    # --------------------------------------------------------------------
+                        # --------------------------------------------------------------------
